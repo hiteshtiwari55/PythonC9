6e1a2a2e1cab73374da102549302effc9d6402fb
hiteshtiwari55 <hitesh.n.u@gmail.com>
2023-01-03 21:32:29 +0000

    Second Commit

--- pytestGit/test_demo1.py
+++ pytestGit/test_demo1.py
@@ -1,5 +1,6 @@
 
 import pytest
+import pandas as pd
 
 def test_first_entry(test_setup):
     print("Before Conftest")
@@ -7,6 +8,8 @@ def test_first_entry(test_setup):
 #@pytest.fixture
 def test_order():
     print("After Conftest")
+    df = pd.DataFrame(range(0, 5))
+    print(df)
 
 
 
