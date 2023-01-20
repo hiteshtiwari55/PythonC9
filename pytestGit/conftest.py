b4ab237a597b72dd908ed285f7feaa7f150b6744
hiteshtiwari55 <118260495+hiteshtiwari55@users.noreply.github.com>
2023-01-20 20:47:35 +0000

    Update conftest.py

--- pytestGit/conftest.py
+++ pytestGit/conftest.py
@@ -15,7 +15,7 @@ AWS_REGION="eu-west-2"
 @pytest.fixture
 def get_aws_sts_assume_role_credentials():
     sts_client = boto3.client("sts")
-    aws_assume_role_arn = "arn:aws:iam::163105429840:role/ht-github-actions-role"
+    aws_assume_role_arn = aws_assume_role_arn
     print(aws_assume_role_arn)
     
     credentials = sts_client.assume_role(
